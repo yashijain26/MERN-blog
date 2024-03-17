a8f9eeb13d4c0b0a27d22ab6633682da9e374e35
yashijain26 <yashiiijainnn@gmail.com>
2024-03-17 11:13:11 +0545

    Sign In page UI

--- react/src/pages/SignIn.jsx
+++ react/src/pages/SignIn.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, { useState}  from "react";
+import { Button, Label, TextInput } from "flowbite-react";
+import { Link,useNavigate } from "react-router-dom";
 
 export const SignIn = () => {
+  const navigate = useNavigate()
+  const[formData,setFormData] =useState({})
+  const handelChange=((e)=>{
+    setFormData({...formData,[e.target.id]:e.target.value})
+  })
+
+  const handelLogin=(()=>{
+    navigate('/')
+  })
+  console.log(formData)
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto">
+        <div className="flex-1">
+          <span>YASH Technologies</span>
+        </div>
+        <div className="fex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="email" />
+              <TextInput
+                type="text"
+                placeholder="enter your email"
+                id="email"
+                onChange={handelChange}
+              />
+            </div>
+            <div>
+              <Label value="password" />
+              <TextInput
+                type="text"
+                placeholder="********"
+                id="password"
+                onChange={handelChange}
+              />
+            </div>
+            <Button gradientDuoTone="purpleToPink" type="submit" onClick={handelLogin}>
+         
+              Sign In
+            </Button>
+          </form>
+          <div className="flex gap-2 mt-5 text-sm">
+            <span>Don't have an account?</span>
+            <Link className="text-blue-500" to="/SignUp">
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- react/src/pages/SignUp.jsx
+++ react/src/pages/SignUp.jsx
@@ -2,52 +2,60 @@ import React, { useState } from "react";
 import { Button, Label, TextInput } from "flowbite-react";
 import { Link } from "react-router-dom";
 
-
 export const SignUp = () => {
-const[formData,setFormData] =useState({})
+  const [formData, setFormData] = useState({});
 
-  const handelChange=((e)=>{
-    setFormData({...formData,[e.target.id]:e.target.value})
-  })
+  const handelChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
 
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto">
-      <div className="flex-1">
-   <span>YASH Technologies</span>
-      </div>
-      <div className="fex-1">
-        <form className="flex flex-col gap-4">
-          <div>
-            <Label value="username" />
-            <TextInput
-              type="text"
-              placeholder="enter your username"
-              id="username"
-              onChange={handelChange}
-            />
-          </div>
-          <div>
-            <Label value="email" />
-            <TextInput type="text" placeholder="enter your email" id="email"  onChange={handelChange}/>
-          </div>
-          <div>
-            <Label value="password" />
-            <TextInput
-              type="text"
-              placeholder="enter your password"
-              id="password"
-              onChange={handelChange}
-            />
-          </div>
-          <Button gradientDuoTone='purpleToPink' type='submit' > Sign Up</Button>
-        </form>
-        <div className="flex gap-2 mt-5 text-sm">
-          <span>Have an account?</span>
-          <Link className="text-blue-500" to='/SignIn'>Sign In</Link>
+        <div className="flex-1">
+          <span>YASH Technologies</span>
         </div>
-      </div>
-
+        <div className="fex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="username" />
+              <TextInput
+                type="text"
+                placeholder="enter your username"
+                id="username"
+                onChange={handelChange}
+              />
+            </div>
+            <div>
+              <Label value="email" />
+              <TextInput
+                type="text"
+                placeholder="enter your email"
+                id="email"
+                onChange={handelChange}
+              />
+            </div>
+            <div>
+              <Label value="password" />
+              <TextInput
+                type="text"
+                placeholder="enter your password"
+                id="password"
+                onChange={handelChange}
+              />
+            </div>
+            <Button gradientDuoTone="purpleToPink" type="submit">
+              {" "}
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 mt-5 text-sm">
+            <span>Have an account?</span>
+            <Link className="text-blue-500" to="/SignIn">
+              Sign In
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
