c21fff781033caf02aa9c54d891d6c4f6e5c8c8b
yashijain26 <yashiiijainnn@gmail.com>
2024-03-18 05:01:15 +0545

    added signUp functionality

--- react/src/pages/SignUp.jsx
+++ react/src/pages/SignUp.jsx
@@ -1,13 +1,42 @@
 import React, { useState } from "react";
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { Link,useNavigate } from "react-router-dom";
 import OAuth from "../components/OAuth";
 
 export const SignUp = () => {
   const [formData, setFormData] = useState({});
-
+   const [errorMessage,setErrorMessage] = useState(null);
+   const [isLoading,setIsLoading] = useState(false)
+ const navigate = useNavigate()
   const handelChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password){
+     return  setErrorMessage('Please fill all the fields')
+    }
+    try {
+      setIsLoading(true)
+      setErrorMessage(null)
+      const res = await fetch("/api/auth/SignUp", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success === false){
+        return setErrorMessage(data.message)
+      }
+      setIsLoading(false)
+      if(res.ok){
+        return navigate('/SignIn')
+      }
+    } catch (error) {
+      setErrorMessage(error.message)
+      setIsLoading(false)
+    }
   };
 
   return (
@@ -17,7 +46,7 @@ export const SignUp = () => {
           <span>YASH Technologies</span>
         </div>
         <div className="fex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handelSubmit}>
             <div>
               <Label value="username" />
               <TextInput
@@ -30,7 +59,7 @@ export const SignUp = () => {
             <div>
               <Label value="email" />
               <TextInput
-                type="text"
+                type="email"
                 placeholder="enter your email"
                 id="email"
                 onChange={handelChange}
@@ -39,17 +68,23 @@ export const SignUp = () => {
             <div>
               <Label value="password" />
               <TextInput
-                type="text"
+                type="password"
                 placeholder="enter your password"
                 id="password"
                 onChange={handelChange}
               />
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">
-        
-              Sign Up
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={isLoading}
+            
+            >
+              {isLoading ?<><Spinner size='sm'
+              /><span className="pl-3">loading...</span></>: 'Sign Up'}
+             
             </Button>
-            <OAuth/>
+            <OAuth />
           </form>
           <div className="flex gap-2 mt-5 text-sm">
             <span>Have an account?</span>
@@ -57,7 +92,8 @@ export const SignUp = () => {
               Sign In
             </Link>
           </div>
-         
+{errorMessage &&
+<Alert className="mt-5" color='failure'>{errorMessage}</Alert>}
         </div>
       </div>
     </div>
